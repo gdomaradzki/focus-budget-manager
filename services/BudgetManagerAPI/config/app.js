eff874eafa341170702110c9e39fa6c5ad78081e
Gustavo Domaradzki <gutzedomaradzki@gmail.com>
2017-10-15 18:28:28 -0200

    Add API app file

---
+++ services/BudgetManagerAPI/config/app.js
@@ -0,0 +1,29 @@
+const express = require('express'),
+      app = express(),
+      bodyParser = require('body-parser'),
+      mongoose = require('mongoose'),
+      morgan = require('morgan'),
+      consign = require('consign'),
+      cors = require('cors'),
+      passport = require('passport'),
+      passportConfig = require('./passport')(passport),
+      jwt = require('jsonwebtoken'),
+      config = require('./config'),
+      database = require('./database')(mongoose, config);
+
+app.use(express.static('.'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(morgan('dev'));
+app.use(cors());
+app.use(passport.initialize());
+
+app.set('secretauth', config.secret);
+
+consign({ cwd: 'BudgetManagerAPI' })
+      .include('app/models')
+      .then('app/api')
+      .then('app/routes')
+      .into(app);
+
+module.exports = app;
