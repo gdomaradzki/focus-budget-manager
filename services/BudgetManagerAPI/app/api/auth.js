4ae1fe5579eb3ca3bd2f8a50e8ab0b8b769d98e8
Gustavo Domaradzki <gutzedomaradzki@gmail.com>
2017-10-15 19:24:02 -0200

    Add API methods

---
+++ services/BudgetManagerAPI/app/api/auth.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose'),
+      jwt = require('jsonwebtoken'),
+      config = require('@config');
+
+const api = {};
+
+api.login = (User) => (req, res) => {
+  User.findOne({ username: req.body.username }, (error, user) => {
+    if (error) throw error;
+
+    if (!user) res.status(401).send({ success: false, message: 'Authentication failed. User not found.' });
+    else {
+      user.comparePassword(req.body.password, (error, matches) => {
+        if (matches && !error) {
+          const token = jwt.sign({ user }, config.secret);
+          res.json({ success: true, message: 'Token granted', token });
+        } else {
+          res.status(401).send({ success: false, message: 'Authentication failed. Wrong password.' });
+        }
+      });
+    }
+  });
+}
+
+api.verify = (headers) => {
+  if (headers && headers.authorization) {
+    const split = headers.authorization.split(' ');
+
+    if (split.length === 2) return split[1];
+    else return null;
+  } else return null;
+}
+
+module.exports = api;
