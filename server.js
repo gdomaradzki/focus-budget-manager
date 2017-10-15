c2d23f5a24342ab748ce23ee443d8d5b32c0305a
Gustavo Domaradzki <gutzedomaradzki@gmail.com>
2017-10-15 14:55:04 -0200

    Restarting project

--- server.js
+++
@@ -1,9 +0,0 @@
-var app = require('./server/config/express'),
-    http = require('http').Server(app),
-    db = require('./server/config/database')('localhost/focus');
-
-const PORT = process.env.PORT || 3000;
-
-http.listen(PORT, () => {
-  console.log('Server running on port ' + PORT);
-});
