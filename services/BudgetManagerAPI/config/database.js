71860c2487e155a85b2cf486f02d8bac9b35acc3
Gustavo Domaradzki <gutzedomaradzki@gmail.com>
2017-10-15 17:49:38 -0200

    Add database connection file

---
+++ services/BudgetManagerAPI/config/database.js
@@ -0,0 +1,20 @@
+module.exports = (mongoose, config) => {
+  const database = mongoose.connection;
+  mongoose.Promise = Promise;
+
+  mongoose.connection(config.database, {
+    useMongoClient: true,
+    promiseLibrary: global.Promise
+  });
+
+  database.on('error', error => console.log(`Connection to BudgetManager database failed: ${error}`));
+  database.on('connected', () => console.log('Connected to BudgetManager database'));
+  database.on('disconnected', () => console.log('Disconnected from BudgetManager database'));
+
+  process.on('SIGINT', () => {
+    database.close(() => {
+      console.log('BudgetManager terminated, connection closed');
+      process.exit(0);
+    })
+  });
+};
