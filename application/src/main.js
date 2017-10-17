290f8394571a57008e397d8c01cb308ccf870aa5
Gustavo Domaradzki <gutzedomaradzki@gmail.com>
2017-10-17 20:22:40 -0200

    Import material icons and vuetify css

--- application/src/main.js
+++ application/src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import Vuetify from 'vuetify'
+import('../node_modules/vuetify/dist/vuetify.min.css')
 
 Vue.use(Vuetify)
 
