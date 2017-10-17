6b38b825c949dc7af8244f594c00e7100bb537de
Gustavo Domaradzki <gutzedomaradzki@gmail.com>
2017-10-17 00:17:18 -0200

    Add background image to application

--- application/src/App.vue
+++ application/src/App.vue
@@ -9,3 +9,22 @@
     name: 'app'
   }
 </script>
+
+<style lang="scss">
+  @import "./assets/styles";
+
+  body {
+    background: url('https://chrismartinphotography.files.wordpress.com/2013/01/2012-landscapes-c2a9-christopher-martin-93441.jpg') no-repeat center center fixed;
+    &:after {
+      content: '';
+      position: fixed;
+      width: 100%;
+      height: 100%;
+      top: 0;
+      left: 0;
+      background-color: $background-color;
+      opacity: .3;
+      z-index: -1;
+    }
+  }
+</style>
