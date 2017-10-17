8e708903b19649ca49ca981ba9d0e98f02f1986f
Gustavo Domaradzki <gutzedomaradzki@gmail.com>
2017-10-16 23:21:48 -0200

    Import authentication component and define his route

--- application/src/router/index.js
+++ application/src/router/index.js
@@ -1,10 +1,17 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 
+// Pages
+import Authentication from '@/components/pages/Authentication'
+
 Vue.use(Router)
 
 export default new Router({
   routes: [
-    {}
+    {
+      path: '/login',
+      name: 'Authentication',
+      component: Authentication
+    }
   ]
 })
