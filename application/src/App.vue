25e4f11ad08e07c50a2620c3a0c2b6cd97630275
Gustavo Domaradzki <gutzedomaradzki@gmail.com>
2017-10-17 19:34:35 -0200

    Change background variable name

--- application/src/App.vue
+++ application/src/App.vue
@@ -22,7 +22,7 @@
       height: 100%;
       top: 0;
       left: 0;
-      background-color: $background-color;
+      background-color: $background-tint;
       opacity: .3;
       z-index: -1;
     }
