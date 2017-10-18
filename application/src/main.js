cfff0b89d71d7d6c58d59265643d3ec32aa02e81
Gustavo Domaradzki <gutzedomaradzki@gmail.com>
2017-10-18 02:13:17 -0200

    Import vuecookie

--- application/src/main.js
+++ application/src/main.js
@@ -1,9 +1,11 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import VueCookie from 'vue-cookie'
 import Vuetify from 'vuetify'
 import('../node_modules/vuetify/dist/vuetify.min.css')
 
+Vue.use(VueCookie)
 Vue.use(Vuetify)
 
 Vue.config.productionTip = false
