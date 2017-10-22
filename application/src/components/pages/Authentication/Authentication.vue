8b2dc240cad8ee523f7485d2eafcd48321df0be6
Gustavo Domaradzki <gutzedomaradzki@gmail.com>
2017-10-22 16:36:50 -0200

    Add sign up method

--- application/src/components/pages/Authentication/Authentication.vue
+++ application/src/components/pages/Authentication/Authentication.vue
@@ -1,34 +1,62 @@
 <template>
-  <div class="l-auth">
-    <v-form v-model="validLogin">
-      <v-text-field label="Username"
-                    v-model="credentials.username"
-                    prepend-icon="account_box"
-                    :rules="rules"
-                    required
-                    color="light-blue lighten-1">
-      </v-text-field>
+  <div class="l-auth-container">
+    <div class="l-auth">
+      <v-form v-model="validLogin">
+        <v-text-field label="Username"
+                      v-model="credentials.username"
+                      prepend-icon="account_box"
+                      :rules="rules"
+                      required
+                      color="light-blue lighten-1">
+        </v-text-field>
 
-      <v-text-field label="Password"
-                    v-model="credentials.password"
-                    prepend-icon="lock"
-                    :rules="rules"
-                    :append-icon="passwordVisible ? 'visibility' : 'visibility_off'"
-                    :append-icon-cb="() => (passwordVisible = !passwordVisible)"
-                    :type="passwordVisible ? 'text' : 'password'"
-                    color="light-blue lighten-1"
-                    required>
-      </v-text-field>
+        <v-text-field label="Password"
+                      v-model="credentials.password"
+                      prepend-icon="lock"
+                      :rules="rules"
+                      :append-icon="loginPasswordVisible ? 'visibility' : 'visibility_off'"
+                      :append-icon-cb="() => (loginPasswordVisible = !loginPasswordVisible)"
+                      :type="loginPasswordVisible ? 'text' : 'password'"
+                      color="light-blue lighten-1"
+                      required>
+        </v-text-field>
 
-      <v-snackbar timeout="6000"
-                  bottom="bottom"
-                  color="red lighten-1"
-                  v-model="snackbar">
-        {{ message }}
-      </v-snackbar>
+        <v-btn flat color="light-blue lighten-1" @click.native="signUpVisible = true">Create account</v-btn>
+        <v-btn color="light-blue lighten-1" @click.native="submitAuthentication()">Login</v-btn>
+      </v-form>
+    </div>
 
-      <v-btn block color="light-blue lighten-1" @click.native="submitAuthentication()">Login</v-btn>
-    </v-form>
+    <div class="l-signup" v-if="signUpVisible">
+      <v-form v-model="validSignUp">
+        <v-text-field label="Username"
+                      v-model="newUser.username"
+                      prepend-icon="account_box"
+                      :rules="rules"
+                      required
+                      color="light-blue lighten-1">
+        </v-text-field>
+
+        <v-text-field label="Password"
+                      v-model="newUser.password"
+                      prepend-icon="lock"
+                      :rules="rules"
+                      :append-icon="signUpPasswordVisible ? 'visibility' : 'visibility_off'"
+                      :append-icon-cb="() => (signUpPasswordVisible = !signUpPasswordVisible)"
+                      :type="signUpPasswordVisible ? 'text' : 'password'"
+                      color="light-blue lighten-1"
+                      required>
+        </v-text-field>
+
+        <v-btn block color="light-blue lighten-1" @click.native="submitSignUp()">Sign Up</v-btn>
+      </v-form>
+    </div>
+
+    <v-snackbar timeout="6000"
+                bottom="bottom"
+                color="red lighten-1"
+                v-model="snackbar">
+      {{ message }}
+    </v-snackbar>
   </div>
 </template>
 
@@ -39,9 +67,16 @@
       return {
         snackbar: false,
         validLogin: false,
-        passwordVisible: false,
+        validSignUp: false,
+        signUpVisible: false,
+        loginPasswordVisible: false,
+        signUpPasswordVisible: false,
         rules: [ (value) => !!value || 'This field is required' ],
         credentials: {
+          username: '',
+          password: ''
+        },
+        newUser: {
           username: '',
           password: ''
         },
@@ -51,6 +86,10 @@
     methods: {
       submitAuthentication () {
         Authentication.authenticate(this, this.credentials, '/')
+      },
+
+      submitSignUp () {
+        Authentication.signup(this, this.newUser, '/')
       }
     }
   }
@@ -66,5 +105,14 @@
     min-width: 272px;
     max-width: 320px;
     animation: bounceIn 1s forwards ease;
+  }
+
+  .l-signup {
+    background-color: $background-color;
+    padding: 15px;
+    margin: 45px auto;
+    min-width: 272px;
+    max-width: 320px;
+    animation: slideInFromLeft 1s forwards ease;
   }
 </style>
--- application/src/components/pages/Authentication/index.js
+++ application/src/components/pages/Authentication/index.js
@@ -9,7 +9,23 @@ export default {
   authenticate (context, credentials, redirect) {
     Axios.post(`${BudgetManagerAPI}/api/v1/auth`, credentials)
         .then(({data: {token}}) => {
-          context.$cookie.set('token', token, '30s')
+          context.$cookie.set('token', token, '1D')
+
+          context.validLogin = true
+
+          this.user.authenticated = true
+
+          if (redirect) router.push(redirect)
+        }).catch(({response: {data}}) => {
+          context.snackbar = true
+          context.message = data.message
+        })
+  },
+
+  signup (context, credentials, redirect) {
+    Axios.post(`${BudgetManagerAPI}/api/v1/signup`, credentials)
+        .then(({data: {token}}) => {
+          context.$cookie.set('token', token, '1D')
 
           context.validLogin = true
 
