91c6c97354edbdcffda220fb28c2479d5ffa21b7
Gustavo Domaradzki <gutzedomaradzki@gmail.com>
2017-10-22 04:18:38 -0200

    Name view model, import Authentication file, add route guard

--- application/src/main.js
+++ application/src/main.js
@@ -3,6 +3,7 @@ import App from './App'
 import router from './router'
 import VueCookie from 'vue-cookie'
 import Vuetify from 'vuetify'
+import Authentication from '@/components/pages/Authentication'
 import('../node_modules/vuetify/dist/vuetify.min.css')
 
 Vue.use(VueCookie)
@@ -11,8 +12,25 @@ Vue.use(Vuetify)
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
-new Vue({
+let vm = new Vue({
   el: '#app',
   router,
   render: h => h(App)
+})
+
+Authentication.checkAuthentication(vm)
+
+router.beforeEach((to, from, next) => {
+  if (to.matched.some(route => route.meta.requiredAuth)) {
+    if (Authentication.user.authenticated) {
+      console.log(Authentication.user.authenticated)
+      next()
+    } else {
+      console.log(Authentication.user.authenticated)
+      router.push('/login')
+      next()
+    }
+  } else {
+    next()
+  }
 })
