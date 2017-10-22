f194fa45e9ad1f0711618d8b57c4aeb64bc49265
Gustavo Domaradzki <gutzedomaradzki@gmail.com>
2017-10-22 01:02:52 -0200

    Add meta for protected routes

--- application/src/router/index.js
+++ application/src/router/index.js
@@ -2,12 +2,21 @@ import Vue from 'vue'
 import Router from 'vue-router'
 
 // Pages
-import Authentication from '@/components/pages/Authentication'
+import Home from '@/components/pages/Home'
+import Authentication from '@/components/pages/Authentication/Authentication'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
+    {
+      path: '/',
+      name: 'Home',
+      component: Home,
+      meta: {
+        requiresAuth: true
+      }
+    },
     {
       path: '/login',
       name: 'Authentication',
