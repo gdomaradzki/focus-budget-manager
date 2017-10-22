b7cc4d554cae43719ed80bb1544a13b74c773ebe
Gustavo Domaradzki <gutzedomaradzki@gmail.com>
2017-10-22 18:14:34 -0200

    Add basic home page for testing

--- application/src/components/pages/Home.vue
+++ application/src/components/pages/Home.vue
@@ -1,9 +1,35 @@
 <template>
   <div>
-    <h1>Hi! this is our App's Home</h1>
+    <h3>Hi! this is our App's Home</h3>
+    <ul>
+      <li v-if="users != null" v-for="user in users">{{ user.username }}</li>
+    </ul>
   </div>
 </template>
 
 <script>
-  export default {}
+  import Axios from 'axios'
+  import Authentication from '@/components/pages/Authentication'
+
+  const BudgetManagerAPI = `http://${window.location.hostname}:3001`
+
+  export default {
+    data () {
+      return {
+        users: []
+      }
+    },
+    mounted () {
+      this.getAllUsers()
+    },
+    methods: {
+      getAllUsers (context) {
+        Axios.get(`${BudgetManagerAPI}/api/v1/users`, {
+          headers: {
+            'Authorization': Authentication.getAuthenticationHeader(this)
+          }
+        }).then(({data}) => (this.users = data))
+      }
+    }
+  }
 </script>
