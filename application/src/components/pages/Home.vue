ba322e70eac070fd7f5184483ffab927a27c80d1
Gustavo Domaradzki <gutzedomaradzki@gmail.com>
2017-10-24 16:34:29 -0200

    Add basic structure to Home page

--- application/src/components/pages/Home.vue
+++ application/src/components/pages/Home.vue
@@ -1,37 +1,34 @@
 <template>
-  <div>
-    <h3>Hi! this is our App's Home</h3>
-    <ul>
-      <li v-if="users != null" v-for="user in users">
-        {{ user.username }}
-      </li>
-    </ul>
-  </div>
+  <main class="l-home-page">
+    <app-header></app-header>
+
+    <div class="l-home">
+      <h4 class="white--text text-xs-center my-0">
+        Focus Budget Manager
+      </h4>
+
+      <budget-list></budget-list>
+    </div>
+  </main>
 </template>
 
 <script>
-  import Axios from 'axios'
-  import Authentication from '@/components/pages/Authentication'
-
-  const BudgetManagerAPI = `http://${window.location.hostname}:3001`
-
   export default {
     data () {
       return {
         users: []
       }
-    },
-    mounted () {
-      this.getAllUsers()
-    },
-    methods: {
-      getAllUsers (context) {
-        Axios.get(`${BudgetManagerAPI}/api/v1/users`, {
-          headers: {
-            'Authorization': Authentication.getAuthenticationHeader(this)
-          }
-        }).then(({data}) => (this.users = data))
-      }
     }
   }
 </script>
+
+<style lang="scss" scoped>
+  @import "./../../assets/styles";
+
+  .l-home {
+    background-color: $background-color;
+    margin: 25px auto;
+    padding: 15px;
+    min-width: 272px;
+  }
+</style>
