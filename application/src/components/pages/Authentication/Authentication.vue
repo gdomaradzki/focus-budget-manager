0f53a0b2a90280eedacc027740d845ad911282f1
Gustavo Domaradzki <gutzedomaradzki@gmail.com>
2017-10-24 19:27:02 -0200

    Change l-signup class styling to extend l-auth

--- application/src/components/pages/Authentication/Authentication.vue
+++ application/src/components/pages/Authentication/Authentication.vue
@@ -119,21 +119,7 @@
   }
 
   .l-signup {
-    background-color: $background-color;
-    padding: 15px;
-    margin: 45px auto;
-    min-width: 272px;
-    max-width: 320px;
+    @extend .l-auth;
     animation: slideInFromLeft 1s forwards ease;
-
-    label, input, .icon {
-      color: #29b6f6!important;
-    }
-
-    .input-group__details {
-      &:before {
-        background-color: $border-color-input !important;
-      }
-    }
   }
 </style>
