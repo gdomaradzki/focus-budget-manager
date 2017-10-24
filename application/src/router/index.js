7c64424926458522a2f200eef87e6d56b87dbb8f
Gustavo Domaradzki <gutzedomaradzki@gmail.com>
2017-10-24 00:04:59 -0200

    Register Header component

--- application/src/router/index.js
+++ application/src/router/index.js
@@ -6,6 +6,12 @@ import * as Auth from '@/components/pages/Authentication'
 import Home from '@/components/pages/Home'
 import Authentication from '@/components/pages/Authentication/Authentication'
 
+// Global components
+import Header from '@/components/Header'
+
+// Register components
+Vue.component('app-header', Header)
+
 Vue.use(Router)
 
 const router = new Router({
@@ -13,7 +19,10 @@ const router = new Router({
     {
       path: '/',
       name: 'Home',
-      component: Home,
+      components: {
+        default: Home,
+        header: Header
+      },
       meta: {
         requiredAuth: true
       }
