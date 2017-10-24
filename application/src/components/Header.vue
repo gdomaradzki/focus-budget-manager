00ab4f735dbe55a5b1870a6457603ed1a8099d40
Gustavo Domaradzki <gutzedomaradzki@gmail.com>
2017-10-23 23:57:27 -0200

    Add Header component

---
+++ application/src/components/Header.vue
@@ -0,0 +1,31 @@
+<template>
+  <header class="l-header-container">
+    <v-text-field v-model="search"
+                  label="Search"
+                  color="teal accent-3"
+                  append-icon="search"
+                  single-line>
+    </v-text-field>
+  </header>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        search: ''
+      }
+    }
+  }
+</script>
+
+<style lang="scss">
+  @import "./../assets/styles";
+
+  .l-header-container {
+    background-color: $background-color;
+    margin: 0 auto;
+    padding: 0 15px;
+    min-width: 272px;
+  }
+</style>
