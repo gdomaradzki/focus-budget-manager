2599471741d01c13c02ef7b008b3d36d49b9de67
Gustavo Domaradzki <gutzedomaradzki@gmail.com>
2017-10-24 22:13:28 -0200

    Instantiate budget model on our setup file

--- services/BudgetManagerAPI/app/setup/index.js
+++ services/BudgetManagerAPI/app/setup/index.js
@@ -1,8 +1,10 @@
 const mongoose = require('mongoose'),
-      UserModel = require('@BudgetManagerModels/user');
+      UserModel = require('@BudgetManagerModels/user'),
+      BudgetModel = require('@BudgetManagerModels/budget');
 
 const models = {
-  User: mongoose.model('User')
+  User: mongoose.model('User'),
+  Budget: mongoose.model('Budget')
 }
 
 module.exports = models;
