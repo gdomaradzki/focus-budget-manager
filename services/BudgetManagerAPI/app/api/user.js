df5d4222b86dc08c4d4a9894cba4a81276b8ab14
Gustavo Domaradzki <gutzedomaradzki@gmail.com>
2017-10-24 23:42:42 -0200

    Remove deprecated routes and methods

--- services/BudgetManagerAPI/app/api/user.js
+++ services/BudgetManagerAPI/app/api/user.js
@@ -2,17 +2,6 @@ const mongoose = require('mongoose');
 
 const api = {};
 
-api.index = (User, BudgetToken) => (req, res) => {
-  const token = BudgetToken;
-
-  if (token) {
-    User.find({}, (error, users) => {
-      if (error) throw error;
-      res.status(200).json(users);
-    });
-  } else return res.status(403).send({ success: false, message: 'Unauthorized' });
-}
-
 api.signup = (User) => (req, res) => {
   if (!req.body.username || !req.body.password) res.json({ success: false, message: 'Please, pass an username and password.' });
   else {
--- services/BudgetManagerAPI/app/routes/user.js
+++ services/BudgetManagerAPI/app/routes/user.js
@@ -5,9 +5,6 @@ const passport = require('passport'),
 module.exports = (app) => {
   const api = app.BudgetManagerAPI.app.api.user;
 
-  app.route('/api/v1/users')
-     .get(passport.authenticate('jwt', config.session), api.index(models.User, app.get('budgetsecret')));
-
   app.route('/api/v1/signup')
      .post(api.signup(models.User));
 }
