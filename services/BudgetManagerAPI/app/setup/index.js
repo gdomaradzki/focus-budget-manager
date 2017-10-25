2a813436ad22eb26c93e7165430ecb9d9e2df335
Gustavo Domaradzki <gutzedomaradzki@gmail.com>
2017-10-24 23:09:11 -0200

    Instantiate Client model on setup file

--- services/BudgetManagerAPI/app/setup/index.js
+++ services/BudgetManagerAPI/app/setup/index.js
@@ -1,10 +1,12 @@
 const mongoose = require('mongoose'),
       UserModel = require('@BudgetManagerModels/user'),
-      BudgetModel = require('@BudgetManagerModels/budget');
+      BudgetModel = require('@BudgetManagerModels/budget'),
+      ClientModel = require('@BudgetManagerModels/client');
 
 const models = {
   User: mongoose.model('User'),
-  Budget: mongoose.model('Budget')
+  Budget: mongoose.model('Budget'),
+  Client: mongoose.model('Client')
 }
 
 module.exports = models;
