fcc2253316c6335085ac82d31b3bf201b65c8e2f
Gustavo Domaradzki <gutzedomaradzki@gmail.com>
2017-10-26 21:37:13 -0200

    Improve User methods

--- services/BudgetManagerAPI/app/api/user.js
+++ services/BudgetManagerAPI/app/api/user.js
@@ -5,13 +5,12 @@ const api = {};
 api.signup = (User) => (req, res) => {
   if (!req.body.username || !req.body.password) res.json({ success: false, message: 'Please, pass an username and password.' });
   else {
-    const newUser = new User({
+    const user = new User({
       username: req.body.username,
-      password: req.body.password,
-      clients: []
+      password: req.body.password
     });
 
-    newUser.save(error => {
+    user.save(error => {
       if (error) return res.status(400).json({ success: false, message: 'Username already exists.' });
       res.json({ success: true, message: 'Account created successfully' });
     });
