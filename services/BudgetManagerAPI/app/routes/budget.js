f2119ec1df081fe7ceeb67acfed4994299b9edd3
Gustavo Domaradzki <gutzedomaradzki@gmail.com>
2017-10-27 17:48:28 -0200

    Add protected routes for budgets and clients

---
+++ services/BudgetManagerAPI/app/routes/budget.js
@@ -0,0 +1,12 @@
+const passport = require('passport'),
+      config = require('@config'),
+      models = require('@BudgetManager/app/setup');
+
+module.exports = (app) => {
+  const api = app.BudgetManagerAPI.app.api.budget;
+
+  app.route('/api/v1/budget')
+     .post(passport.authenticate('jwt', config.session), api.store(models.User, models.Budget, app.get('budgetsecret')))
+     .get(passport.authenticate('jwt', config.session), api.getAll(models.User, models.Budget, app.get('budgetsecret')))
+     .get(passport.authenticate('jwt', config.session), api.getAllFromClient(models.User, models.Budget, app.get('budgetsecret')))
+}
---
+++ services/BudgetManagerAPI/app/routes/client.js
@@ -0,0 +1,12 @@
+const passport = require('passport'),
+      config = require('@config'),
+      models = require('@BudgetManager/app/setup');
+
+module.exports = (app) => {
+  const api = app.BudgetManagerAPI.app.api.client;
+
+  app.route('/api/v1/client')
+     .post(passport.authenticate('jwt', config.session), api.store(models.User, models.Client, app.get('budgetsecret')))
+     .get(passport.authenticate('jwt', config.session), api.getAll(models.User, models.Client, app.get('budgetsecret')));
+
+}
