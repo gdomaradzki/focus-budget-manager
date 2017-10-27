351999b22c622cdaf8354528fa36f63e24ad00cd
Gustavo Domaradzki <gutzedomaradzki@gmail.com>
2017-10-27 17:47:52 -0200

    Added user to login response

--- services/BudgetManagerAPI/app/api/auth.js
+++ services/BudgetManagerAPI/app/api/auth.js
@@ -13,7 +13,7 @@ api.login = (User) => (req, res) => {
       user.comparePassword(req.body.password, (error, matches) => {
         if (matches && !error) {
           const token = jwt.sign({ user }, config.secret);
-          res.json({ success: true, message: 'Token granted', token });
+          res.json({ success: true, message: 'Token granted', token, user: user });
         } else {
           res.status(401).send({ success: false, message: 'Authentication failed. Wrong password.' });
         }
