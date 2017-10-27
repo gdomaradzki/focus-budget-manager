1d6c8f9347734df303f654cf5109f3eaca3b2a64
Gustavo Domaradzki <gutzedomaradzki@gmail.com>
2017-10-27 18:02:16 -0200

    Add sign out button

--- application/src/components/Header.vue
+++ application/src/components/Header.vue
@@ -1,7 +1,7 @@
 <template>
   <header class="l-header-container">
     <v-layout row wrap>
-      <v-flex xs12 md6>
+      <v-flex xs12 md5>
         <v-text-field v-model="search"
                       label="Search"
                       append-icon="search"
@@ -9,7 +9,7 @@
         </v-text-field>
       </v-flex>
 
-      <v-flex xs12 offset-md2 md1>
+      <v-flex xs12 offset-md1 md1>
         <v-btn block color="light-blue lighten-1">Clients</v-btn>
       </v-flex>
 
@@ -21,11 +21,16 @@
                   single-line>
         </v-select>
       </v-flex>
+
+      <v-flex xs12 offset-md1 md1>
+        <v-btn block color="red lighten-1 white--text" @click.native="submitSignout()">Sign out</v-btn>
+      </v-flex>
     </v-layout>
   </header>
 </template>
 
 <script>
+  import Authentication from '@/components/pages/Authentication'
   export default {
     data () {
       return {
@@ -34,6 +39,11 @@
         statusItems: [
           'All', 'Approved', 'Denied', 'Waiting', 'Writing', 'Editing'
         ]
+      }
+    },
+    methods: {
+      submitSignout () {
+        Authentication.signout(this, '/login')
       }
     }
   }
