f2c6d269a586a9e253a4a93df043bea54915d711
Gustavo Domaradzki <gutzedomaradzki@gmail.com>
2017-10-30 23:43:44 -0200

    Make it able to render clients and budgets

--- application/src/components/pages/Home.vue
+++ application/src/components/pages/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <main class="l-home-page">
-    <app-header></app-header>
+    <app-header :budgetsVisible="budgetsVisible"></app-header>
 
     <div class="l-home">
       <h4 class="white--text text-xs-center my-0">
@@ -8,8 +8,8 @@
       </h4>
 
       <list>
-        <list-header slot="list-header" :headers="budgetHeaders"></list-header>
-        <list-body slot="list-body" :data="budgets"></list-body>
+        <list-header slot="list-header" :headers="budgetsVisible ? budgetHeaders : clientHeaders"></list-header>
+        <list-body slot="list-body" :data="budgetsVisible ? budgets : clients"></list-body>
       </list>
     </div>
 
@@ -38,7 +38,10 @@
     data () {
       return {
         budgets: [],
+        clients: [],
         budgetHeaders: ['Client', 'Title', 'Status', 'Actions'],
+        clientHeaders: ['Client', 'Email', 'Phone', 'Actions'],
+        budgetsVisible: true,
         snackbar: false,
         timeout: 6000,
         message: ''
@@ -46,6 +49,7 @@
     },
     mounted () {
       this.getAllBudgets()
+      this.getAllClients()
     },
     methods: {
       getAllBudgets () {
@@ -53,7 +57,19 @@
           headers: { 'Authorization': Authentication.getAuthenticationHeader(this) },
           params: { user_id: this.$cookie.get('user_id') }
         }).then(({data}) => {
-          this.dataParser(data, '_id', 'client', 'title', 'state')
+          this.budgets = this.dataParser(data, '_id', 'client', 'title', 'state')
+        }).catch(error => {
+          this.snackbar = true
+          this.message = error.message
+        })
+      },
+
+      getAllClients () {
+        Axios.get(`${BudgetManagerAPI}/api/v1/client`, {
+          headers: { 'Authorization': Authentication.getAuthenticationHeader(this) },
+          params: { user_id: this.$cookie.get('user_id') }
+        }).then(({data}) => {
+          this.clients = this.dataParser(data, '_id', 'client', 'email', 'phone')
         }).catch(error => {
           this.snackbar = true
           this.message = error.message
@@ -61,11 +77,13 @@
       },
 
       dataParser (targetedArray, ...options) {
+        let parsedData = []
         targetedArray.forEach(item => {
-          let parsed = {}
-          options.forEach(option => (parsed[option] = item[option]))
-          this.budgets.push(parsed)
+          let parsedItem = {}
+          options.forEach(option => (parsedItem[option] = item[option]))
+          parsedData.push(parsedItem)
         })
+        return parsedData
       }
     }
   }
