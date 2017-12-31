2f84d233aa8602680134ec35a4df8148e85b66e0
Gustavo Domaradzki <gutzedomaradzki@gmail.com>
2017-12-31 04:01:07 -0200

    Fix client store method

--- services/BudgetManagerAPI/app/api/client.js
+++ services/BudgetManagerAPI/app/api/client.js
@@ -4,17 +4,26 @@ const api = {};
 
 api.store = (User, Client, Token) => (req, res) => {
   if (Token) {
-    const client = new Client({
-      user_id: req.body.user_id,
-      name: req.body.name,
-      email: req.body.email,
-      phone: req.body.phone,
-    });
+    User.findOne({ _id: req.query.user_id }, (error, user) => {
+      if (error) res.status(400).json(error);
 
-    client.save(error => {
-      if (error) return res.status(400).json(error);
-      res.status(200).json({ success: true, message: "Client registration successfull" });
+      if (user) {
+        const client = new Client({
+          user_id: req.query.user_id,
+          name: req.body.name,
+          email: req.body.email,
+          phone: req.body.phone,
+        });
+
+        client.save(error => {
+          if (error) return res.status(400).json(error);
+          res.status(200).json({ success: true, message: "Client registration successfull" });
+        })
+      } else {
+        res.status(400).json({ success: false, message: "Invalid client" })
+      }
     })
+
   } else return res.status(403).send({ success: false, message: 'Unauthorized' });
 }
 
