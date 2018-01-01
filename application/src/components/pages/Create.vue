7485a7dfa6cd92616c833ba7f7bbcda0ea81748f
Gustavo Domaradzki <gutzedomaradzki@gmail.com>
2018-01-01 16:38:39 -0200

    Add Create page

---
+++ application/src/components/pages/Create.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="l-create-page">
+    <budget-creation v-if="budgetCreation && !editPage" slot="budget-creation" :clients="clients" :saveBudget="saveBudget"></budget-creation>
+    <client-creation v-if="!budgetCreation && !editPage" slot="client-creation" :saveClient="saveClient"></client-creation>
+
+    <budget-edit v-else-if="budgetEdit && editPage"
+      slot="budget-creation"
+      :clients="clients"
+      :selectedBudget="budget"
+      :fixClientNameAndUpdate="fixClientNameAndUpdate">
+    </budget-edit>
+
+    <client-edit v-else-if="!budgetEdit && editPage"
+      slot="client-creation"
+      :selectedClient="client"
+      :updateClient="updateClient">
+    </client-edit>
+  </div>
+</template>
+
+<script>
+  import BudgetCreation from './../Creation/BudgetCreation'
+  import ClientCreation from './../Creation/ClientCreation'
+  import BudgetEdit from './../Creation/BudgetEdit'
+  import ClientEdit from './../Creation/ClientEdit'
+  export default {
+    props: [
+      'budgetCreation', 'clients', 'saveBudget',
+      'saveClient', 'budget', 'client', 'updateClient',
+      'fixClientNameAndUpdate', 'editPage', 'budgetEdit'
+    ],
+    components: {
+      'budget-creation': BudgetCreation,
+      'client-creation': ClientCreation,
+      'budget-edit': BudgetEdit,
+      'client-edit': ClientEdit
+    }
+  }
+</script>
